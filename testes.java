981ee1b85eee35401fcf70413bfd12e1a7d10d8e
Adrian Gabriel <adrian.gabriel22@gmail.com>
2022-03-24 20:37:13 -0300

    commit main

---
+++ testes.java
@@ -0,0 +1,26 @@
+
+// import java.awt.Graphics;
+// import java.awt.Graphics2D;
+// import java.awt.GraphicsEnvironment;
+import javax.swing.JFrame;
+
+public class testes {
+
+	public static void main(String[] args) {
+		// Cliente c = new Cliente();
+
+		// c.setCpf("6546546546546");
+		// c.setNome("Carlinhos");
+		// c.setDataNascimento("22", "03", "2001");
+		// System.out.println(c.getId());
+		// System.out.println(c.getCpf());
+		// System.out.println(c.getNome());
+		JFrame janela = new JFrame("Teste");
+		janela.setSize(1250, 800);
+		janela.setVisible(true);
+		janela.setLocationRelativeTo(null);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+	}
+}
